d7363221409f6529132b72cf46032487e54efc8c
TravisLeblanc1 <travis.leblanc3002@gmail.com>
2024-10-06 09:40:24 +0200

    first real commit

---
+++ tests/trees-nested.ml
@@ -0,0 +1,26 @@
+
+type tree = E | N of tree * tree
+
+let is_empty (t: tree) = match t with
+  | E -> true
+  | N(t1, t2) -> false
+in
+let is_singleton (t: tree) = match t with
+  | E -> false
+  | N(E, E) -> true
+  | N(t1, t2) -> false
+in
+let rec size (t: tree): int = match t with
+  | E -> 1
+  | N(E, E) -> 3
+  | N(N(t1, t2),N(t3, t4)) -> 3 + (size t1) + (size t2) +(size t3) + (size t4)
+  | N(t1, t2) -> 1 + (size t1) + (size t2)
+in
+
+let max (x: int) (y: int) = if x < y then y else x in
+let rec height (t: tree): int = match t with
+  | E -> 0
+  | N(t1, t2) -> 1 + max (height t1) (height t2)
+in
+
+let t = N(N(E,E), N(N(E,E), N(E,E))) in size t (*expect 11*)
